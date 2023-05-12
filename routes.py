5746d3c1653131e9f12e7c3e2767de8509334098
Linus Ling <xiaoxuling@outlook.com>
2023-05-12 12:44:38 +1200

    added messages

--- routes.py
+++ routes.py
@@ -29,24 +29,29 @@ def all_frogs():
     cur = conn.cursor()
     cur.execute('SELECT * FROM frogs')
     frogs = cur.fetchall()
+    print(frogs)
     return render_template("all_frogs.html", frogs=frogs)
 
 @app.route('/frog/<int:id>')
 def frog(id):
     conn = sqlite3.connect('frog.db')
     cur = conn.cursor()
+
     cur.execute('SELECT * FROM frogs WHERE id=?', (id,))
     frog = cur.fetchone()
+
     cur.execute('SELECT * FROM country WHERE id IN(SELECT cid FROM FrogCountry WHERE fid =?)', (id,))
     country = cur.fetchall()
+
     cur.execute('SELECT * FROM prey WHERE id IN(SELECT pid FROM FrogPrey WHERE fid =?)', (id,))
     prey = cur.fetchall()
+
     cur.execute('SELECT * FROM predator WHERE id IN(SELECT pid FROM FrogPredator WHERE fid =?)', (id,))
     predator = cur.fetchall()
+
     cur.execute('SELECT * FROM habitat WHERE id IN(SELECT hid FROM FrogHabitat WHERE fid =?)', (id,))
     habitat = cur.fetchall()
-    #cur.execute('SELECT * FROM country WHERE id =?', (countryid[1],))
-    #country = cur.fetchall()
+
     return render_template("frog.html", frog = frog, country = country, prey = prey, predator = predator, habitat = habitat)
 
 if __name__ == "__main__": 
