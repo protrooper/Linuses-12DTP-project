4dc1b144e38d7af08177b1e14d743642d6194f96
Linus Ling <xiaoxuling@outlook.com>
2023-08-02 22:42:04 +1200

    finished insert_data function, created fetch_data function to run SQL queries.

--- routes.py
+++ routes.py
@@ -1,12 +1,8 @@
 from flask import Flask, render_template, request
 import sqlite3
-
 import random
-
 import os 
-
 from werkzeug.utils import secure_filename
-
 
 
 #where uploaded images go
@@ -17,6 +13,32 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+#executes a query/multiple queries
+# parameters: 
+#       queries (list): a list of list/s containing the SQL queries and their parameters
+#       fetchall (list): a list of bool values which determine whether the fetchall is used or not       
+
+def fetch_data(queries:list, fetchall:list):
+    conn = sqlite3.connect('frog.db')
+    cur = conn.cursor()
+    results = []
+    
+    for index, query in enumerate(queries):
+        try:                                                 # checks if query[1] index exists, thefourtheye - https://stackoverflow.com/questions/29715501/how-can-i-check-if-a-list-index-exists
+            cur.execute(query[0], query[1])              
+        except IndexError:
+            print("index don't exist!")
+            cur.execute(query[0])
+
+        if fetchall[index] is True:
+            result = cur.fetchall()
+        else:
+            result = cur.fetchone()
+        results.append(result)
+
+    conn.close()
+    return results
 
 #inserts data into database, takes parameters from form
 def insertdata(name, image, description, countries, habitats, preys, predators):
@@ -29,47 +51,31 @@ def insertdata(name, image, description, countries, habitats, preys, predators):
 
     insert_into(countries, name, 'country', 'FrogCountry', 'cid', cur)
     insert_into(habitats, name, 'habitat', 'FrogHabitat', 'hid', cur)
-    insert_into(preys, name, 'country', 'FrogPrey', 'pid', cur)
-    insert_into(predators, name, 'country', 'FrogPredator', 'pid', cur)
-    # for country in countries:
-    #     #check if country exists in db
-    #     cur.execute('SELECT * FROM country WHERE name =?', (country,))
-    #     if cur.fetchone() is None:
-    #         #if country doesn't exist, add new country to db
-    #         cur.execute('INSERT INTO country (name) VALUES (?)', (country,))
-    #     cur.execute('INSERT INTO FrogCountry (fid, cid) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM country WHERE name=?))', (name, country))
-        
-    # for habitat in habitats:
-    #     cur.execute('SELECT * FROM habitat WHERE name =?', (habitat,))
-    #     if cur.fetchone() is None:
-    #         cur.execute('INSERT INTO habitat (name) VALUES (?)', (habitat,))
-    #     cur.execute('INSERT INTO FrogHabitat (fid, hid) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM habitat WHERE name=?))', (name, habitat))
-
-    # for prey in preys:
-    #     cur.execute('SELECT * FROM prey WHERE name =?', (prey,))
-    #     if cur.fetchone() is None:
-    #         cur.execute('INSERT INTO prey (name) VALUES (?)', (prey,))
-    #     cur.execute('INSERT INTO FrogPrey (fid, pid) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM prey WHERE name=?))', (name, prey))
-
-    # for predator in predators:
-    #     cur.execute('SELECT * FROM predator WHERE name =?', (predator,))
-    #     if cur.fetchone() is None:
-    #         cur.execute('INSERT INTO predator (name) VALUES (?)', (predator,))
-    #     cur.execute('INSERT INTO FrogPredator (fid, pid) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM predator WHERE name=?))', (name, predator))
+    insert_into(preys, name, 'prey', 'FrogPrey', 'pid', cur)
+    insert_into(predators, name, 'predator', 'FrogPredator', 'pid', cur)
 
     conn.commit()
     conn.close()
 
 
-#for inserting data into tables which contain foreign keys. parameters determine which table/s data is inserted into.
-def insert_into(data, frogName, table, jointTable, id, cur):
-    for item in data:
+#for inserting data into tables which contain foreign keys. 
+# parameters:
+#       data(list): data to be inserted
+#       frogName(str): name of frog
+#       tableName(str): name of table new data is inserted into 
+#       jointTable(str): table where id of frog is matched with id of data from tableName
+#       id(str): name of column in joinTable which is to be matched with fid.
+#       cur: cursor for executing queries
+
+def insert_into(data, frogName, tableName, jointTable, id, cur):
+    items = set(data)                      #make sure there is no duplicate data,  Denis Otkidach- https://stackoverflow.com/questions/1541797/how-do-i-check-if-there-are-duplicates-in-a-flat-list
+    for item in items:
         #check if data exists in table
-        cur.execute('SELECT * FROM ? WHERE name =?', (table, item,))
+        cur.execute(f'SELECT * FROM {tableName} WHERE name =?', (item,))
         if cur.fetchone() is None:
             #if data doesn't exist, add data to table
-            cur.execute('INSERT INTO ? (name) VALUES (?)', (table, item,))
-        cur.execute('INSERT INTO ? (fid, ?) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM ? WHERE name=?))', (jointTable, id, frogName, table, item))
+            cur.execute(f'INSERT INTO {tableName} (name) VALUES (?)', (item,))
+        cur.execute(f'INSERT INTO {jointTable} (fid, {id}) VALUES ((SELECT id FROM frogs WHERE name =?), (SELECT id FROM {tableName} WHERE name=?))', (frogName, item))
 
 
 #searches database for frogs which match the searched criteria, takes parameters from form.
@@ -128,86 +134,45 @@ def explore():
 
     else:
         sortedResults = []
-        conn = sqlite3.connect('frog.db')
-        cur = conn.cursor()
-
-        cur.execute('SELECT * FROM country')
-        countries = cur.fetchall()
-
-        cur.execute('SELECT * FROM habitat')
-        habitats = cur.fetchall()
-
-        cur.execute('SELECT * FROM prey')
-        preys = cur.fetchall()
-
-        cur.execute('SELECT * FROM predator')
-        predators = cur.fetchall()
-
-        conn.close()
+        queries = [['SELECT * FROM country'],
+                   ['SELECT * FROM habitat'],
+                   ['SELECT * FROM prey'],
+                   ['SELECT * FROM predator']]
+        fetchall = [True, True, True, True]
+        countries, habitats, preys, predators = fetch_data(queries, fetchall)
         
         return render_template("explore.html", title="Explore", results=sortedResults, countries=countries, habitats=habitats, preys=preys, predators=predators)
 
 
 @app.route('/all_frogs')
 def all_frogs():
-    conn = sqlite3.connect('frog.db')
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM frogs')
-    frogs = cur.fetchall()
-
-    sortedFrogs = sorted(frogs, key=lambda frog: frog[1])  #sort in alphabetical order
-
-    conn.close()
-
-    return render_template("all_frogs.html", frogs=sortedFrogs)
+    queries = [['SELECT * FROM frogs ORDER BY name']]
+    frogs = fetch_data(queries, [True])
+    return render_template("all_frogs.html", frogs=frogs[0])
 
 
 @app.route('/frog/<int:id>')
 def frog(id):
-    conn = sqlite3.connect('frog.db')
-    cur = conn.cursor()
-
-    cur.execute('SELECT * FROM frogs WHERE id=?', (id,))
-    frog = cur.fetchone()
-
-    cur.execute('SELECT * FROM country WHERE id IN(SELECT cid FROM FrogCountry WHERE fid =?)', (id,))
-    country = cur.fetchall()
-
-    cur.execute('SELECT * FROM prey WHERE id IN(SELECT pid FROM FrogPrey WHERE fid =?)', (id,))
-    prey = cur.fetchall()
-
-    cur.execute('SELECT * FROM predator WHERE id IN(SELECT pid FROM FrogPredator WHERE fid =?)', (id,))
-    predator = cur.fetchall()
-
-    cur.execute('SELECT * FROM habitat WHERE id IN(SELECT hid FROM FrogHabitat WHERE fid =?)', (id,))
-    habitat = cur.fetchall()
-
-    conn.close()
+    queries = [['SELECT * FROM frogs WHERE id=?', (id,)], 
+               ['SELECT * FROM country WHERE id IN(SELECT cid FROM FrogCountry WHERE fid =?)', (id,)], 
+               ['SELECT * FROM prey WHERE id IN(SELECT pid FROM FrogPrey WHERE fid =?)', (id,)],
+               ['SELECT * FROM predator WHERE id IN(SELECT pid FROM FrogPredator WHERE fid =?)', (id,)],
+               ['SELECT * FROM habitat WHERE id IN(SELECT hid FROM FrogHabitat WHERE fid =?)', (id,)]]
+    fetchall = [False, True, True, True, True, True]
+    frog, country, prey, predator, habitat = fetch_data(queries, fetchall)
 
     return render_template("frog.html", frog=frog, country=country, prey=prey, predator=predator, habitat=habitat)
 
 
 @app.route('/insert', methods=['GET', 'POST'])
 def insert():
-    conn = sqlite3.connect('frog.db')
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM frogs')
-    frogs = cur.fetchall()
-
-    cur.execute('SELECT * FROM country')
-    countries = cur.fetchall()
-
-    cur.execute('SELECT * FROM habitat')
-    habitats = cur.fetchall()
-
-    cur.execute('SELECT * FROM prey')
-    preys = cur.fetchall()
-
-    cur.execute('SELECT * FROM predator')
-    predators = cur.fetchall()
-
-    conn.close() 
+    queries = [['SELECT * FROM frogs'],
+               ['SELECT * FROM country'],
+               ['SELECT * FROM habitat'],
+               ['SELECT * FROM prey'],
+               ['SELECT * FROM predator']]
+    fetchall = [True, True, True, True, True]
+    frogs, countries, habitats, preys, predators = fetch_data(queries, fetchall)
 
     if request.method == "POST":
 
